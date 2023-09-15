d207d49433561c66ca3d3d40ae9b64ade649105f
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2023-09-15 18:19:44 +0530

    Problem solved Permutation of a string

---
+++ Recursion_Day9.cpp
@@ -0,0 +1,33 @@
+"In this i solve the problem named as Permutation of a string on Leetcode."
+
+
+// Code
+/* 
+
+class Solution {
+    private:
+    void solve(vector<int> nums, int index, vector<vector<int>>& ans){
+        // Base case
+        if(index >= nums.size()){
+            ans.push_back(nums);
+            return ;
+        }
+
+        for(int j = index; j < nums.size(); j++){
+            swap(nums[index], nums[j]);
+            solve(nums, index + 1, ans);
+            // Backtracking
+            swap(nums[index], nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(nums, index, ans);
+        return ans;
+
+    }
+};
+
+ */
