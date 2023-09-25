d88acdfdd2ef79fd6c336e59cccc1fac3c8ca959
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2023-09-25 20:02:46 +0530

    Learn some more concepts about OOPS & added it's code.

--- OOPS_DAY1.cpp
+++ OOPS_DAY1.cpp
@@ -12,7 +12,7 @@ Q. How to access the Data members?
 
 --> There are 3 access modifier in Cpp : 1) public - we can access the data members from anywhere.
                                         2) private - we can access the data members only inside the class.
-                                        3) protected
+                                        3) protected - 
 
 --> We can access or read or manipulate a private data member outside the class with the help of Getter() && Setter().
 
@@ -26,11 +26,30 @@ Q. How to access the Data members?
 --> 'this' is a keyword in cpp , which store the address of current object. So that means we can observe that,
     'this' is a pointer , which points to the address location of the current object.
 
-*/
+--> Default copy constructor does shallow copy , where as custom copy constructor does Deep copy. In shallow copy we access the 
+    same memory location with 2 different names but in Deep copy another copy is made of the given data & the new variable points
+    to the copied data. 
+    
+--> Copy assignment operator "=": If object is already created & now you want to copy the value of one obj to another obj then 
+                                    you will use the copy assignment operator to do so. Ex: a = b that means copy the values of b to a.
+    
+--> Destructor = It is used to free the memory. When the scope of a variable ends then the destructor is called to free up the memory.
+                In case of statically created obj the destructor is automatically called but in case of dynamically created obj we have
+                to call the destructor manually.
+    
+--> Static keyword: It creates a data member which belongs to a class & to access the member we don't need to create an object.
+                    As long as you create a static member you have to initialize it from outside the class.
+                    The syntax for initializing it is: [datatype className :: nameOfMember = value;] , where :: -> scope resolution operator
+    
+--> Static functions: No need to creating obj for accesing it. We can access it by it's class name only. They don't have 'this' keyword.
+                    It only access the static members only. If we try to access the non-static members in this function then it will 
+                    generate error.
+    */
 
 
 
 #include<iostream>
+#include<cstring>
 using namespace std;
 class Hero
 {
@@ -40,10 +59,13 @@ class Hero
 
     public:
     char level;
+    char *name;
+    static int complete;
 
     Hero()
     {
         cout << "I'm Constructor. " << endl;
+        name = new char[100];
     }
 
     Hero(int health)
@@ -60,6 +82,9 @@ class Hero
     // If we write manual copy constructor then the automatic copy constructor will die.
     Hero(Hero& mom)
     {
+        char *ch = new char[strlen(mom.name) + 1];
+        strcpy(ch, mom.name);
+        this -> name = ch;
         cout << "Manual copy constructor called." << endl;
         this -> health = mom.health;
         this -> level = mom.level;
@@ -67,9 +92,11 @@ class Hero
 
     void print()
     {
-        cout << "Health is " << this -> health << endl;
-        cout << "Level is " << this -> level << endl;
         cout << endl;
+        cout << " [ Health: " << this -> health << ", ";
+        cout << "Level: " << this -> level << ", ";
+        cout << "Name: " << this -> name << " ]";
+        cout << endl << endl;
     }
 
     int getHealth()
@@ -89,13 +116,85 @@ class Hero
     {
         level = f;
     }
+
+    void setName(char name[])
+    {
+        strcpy(this -> name, name);
+    }
+
+    // Creating a static function
+    static int solve()
+    {
+        return complete;
+    }
+
+    // Created Destructor
+    ~Hero()
+    {
+        cout << "Hi, I'm Destructor." << endl;
+    }
+
 };
 
+// Initializing the static member
+int Hero::complete = 6;
 
 int main()
 {
 
+    // Accessing the static function
+    cout << Hero::solve << endl;
 
+
+
+    // Accessing the static member
+    cout << Hero::complete << endl;
+
+
+
+/* 
+
+    // Concept of Destructor
+
+    // Static
+    Hero a;
+
+    // Dynamic
+    Hero *b = new Hero();
+    delete b;
+
+    */
+
+
+
+/* 
+
+    // Concept of shallow & deep copy
+
+    Hero h1;
+    h1.setHealth(78);
+    h1.setLevel('O');
+    char name[7] = "SUBHAM";
+    h1.setName(name);
+    h1.print();
+
+    Hero h2(h1);
+    h2.print();
+
+    h1.name[0] = 'A';
+    h1.print();
+    h2.print();
+
+    // (In the case of default copy constructor)
+    // Here h2 also changes coz we define our variable "name"in the above as a pointer type.
+    // So as h1 changes so h2 also changes.
+    // So this concept is known is shallow copy.
+
+    */
+
+
+
+/*
 
     Hero y(67, 'O');
     y.print();
@@ -104,11 +203,10 @@ int main()
     Hero p(y);
     p.print();
 
+*/
 
 
 
-
-    
     /*
     
     Hero kanha(89);
