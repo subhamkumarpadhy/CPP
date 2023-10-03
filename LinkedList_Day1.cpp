acda1d29129a715e6341b252c60f39b0dff8cab0
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2023-10-03 23:00:35 +0530

    Learned about insertion & delection at any position in Doubly Linked List

--- LinkedList_Day1.cpp
+++ LinkedList_Day1.cpp
@@ -1,3 +1,6 @@
+/*
+    // Concepts of singly linked list
+
 #include<iostream>
 using namespace std;
 
@@ -165,3 +168,203 @@ int main() {
     return 0;
     
 }
+
+*/
+
+
+/*
+    // Concepts of Doubly Linked List
+    
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* pre;
+    Node* next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> pre = NULL;
+        this -> next = NULL;
+    }
+
+    ~Node() {
+        int val = this -> data;
+        if (next != NULL)
+        {
+            delete next;
+            next = NULL;
+        }
+        cout << "Memory free for node with value " << val << endl;
+    }
+};
+
+void print(Node* &head) {
+
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+void insertAtHead(Node* &tail, Node* &head, int data) {
+
+    // To handle the empty list case
+    if (head == NULL)
+    {
+        Node* temp = new Node(data);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node* temp = new Node(data);
+        temp -> next = head;
+        head -> pre = temp;
+        head = temp;
+    }
+}
+
+void insertAtTail(Node* &tail, Node* &head, int data) {
+
+    // To handle the empty list case
+    if (head == NULL)
+    {
+        Node* temp = new Node(data);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node* temp = new Node(data);
+        tail -> next = temp;
+        temp -> pre = tail;
+        tail = temp;
+    }
+}
+
+
+void insertAtMiddle(Node* &tail, Node* &head, int position, int data) {
+    // Insearting at start case handle
+    if (position == 1)
+    {
+        insertAtHead(tail, head, data);
+        return;
+    }
+    
+    Node* temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp -> next;
+        cnt++;
+    }
+
+    // Insearting at tail case handle
+    if (temp -> next == NULL)
+    {
+        insertAtTail(tail, head, data);
+        return;
+    }
+    
+    
+     // Creating a new node
+    Node* nodeToInsert = new Node(data);
+
+    nodeToInsert -> next = temp -> next;
+    temp -> next -> pre = nodeToInsert;
+    temp -> next = nodeToInsert;
+    nodeToInsert -> pre = temp;
+
+}
+
+// Function to get the length of a Linked List
+int getLength(Node* &head) {
+    int length = 0;
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        length++;
+        temp = temp -> next;
+    }
+    return length;
+}
+
+
+void deleteNode(int position, Node* &head) {
+
+    // Deleting the first node
+    if (position == 1) {
+        Node* temp = head;
+        temp -> next -> pre = NULL;
+        head = temp -> next;
+        temp -> next = NULL;
+        delete temp;
+    }
+
+    // Deleting at any middle node or last node
+    else {
+
+        // Creating 2 nodes
+        Node* curr = head;
+        Node* pre = NULL;
+
+        int cnt = 1;
+        while (cnt < position)
+        {
+            pre = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+
+        curr -> pre = NULL;
+        pre -> next = curr -> next;
+        curr -> next = NULL;
+        delete curr;
+
+    }
+}
+
+
+int main() {
+    
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    Node* tail = node1;
+    print(head);
+    cout << "The length of the linkedlist is " << getLength(head) << endl;
+
+    // insertAtHead(tail, head, 8);
+    // print(head);
+    // insertAtHead(tail, head, 5);
+    // print(head);
+    // insertAtHead(tail, head, 2);
+    // print(head);
+
+    insertAtTail(tail, head, 12);
+    print(head);
+    insertAtTail(tail, head, 15);
+    print(head);
+    insertAtTail(tail, head, 22);
+    print(head);
+
+
+    insertAtMiddle(tail, head, 2, 26);
+    print(head);
+    insertAtMiddle(tail, head, 1, 45);
+    print(head);
+    insertAtMiddle(tail, head, 5, 86);
+    print(head);
+
+    deleteNode(, head);
+    print(head);
+
+    return 0;
+}
+
+*/
