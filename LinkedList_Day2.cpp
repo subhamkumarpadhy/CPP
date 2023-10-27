bb13e927ed2b9250c12ec9ed7a437b970d60c8ba
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2023-10-27 10:39:05 +0530

    Add code of find middle element in linkedlist

--- LinkedList_Day2.cpp
+++ LinkedList_Day2.cpp
@@ -1,23 +1,24 @@
 Solved two problems on code studio
 
+Q.Reverse the linked list->sloved with 3 approaches
 
-Q. Reverse the linked list -> sloved with 3 approaches
+        // Approach - 3
 
-    // Approach - 3
-
-Node* reverse1(Node* &head) {
-    if(head == NULL || head -> next == NULL) {
+        Node *
+        reverse1(Node *&head)
+{
+    if (head == NULL || head->next == NULL)
+    {
         return head;
     }
 
-    Node* chotaHead = reverse1(head -> next);
+    Node *chotaHead = reverse1(head->next);
 
-    head -> next -> next = head;
-    head -> next = NULL;
+    head->next->next = head;
+    head->next = NULL;
 
     return chotaHead;
 }
-
 
 /*
     // Approach - 2
@@ -35,7 +36,8 @@ void reverse(Node* &head, Node* curr, Node* pre) {
 
 */
 
-Node *reverseLinkedList(Node *head) {
+Node *reverseLinkedList(Node *head)
+{
 
     return reverse1(head);
 
@@ -50,10 +52,8 @@ Node *reverseLinkedList(Node *head) {
 
     */
 
-
-
     /*
-    
+
     // Approach - 1
 
     if(head == NULL || head -> next == NULL) {
@@ -71,6 +71,63 @@ Node *reverseLinkedList(Node *head) {
         curr = forward;
     }
     return pre;
+
+    */
+}
+
+Q.Find the middle element of the given LinkedList
+
+Node * getMiddle(Node *head) {
+
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    Node *slow = head;
+    Node *fast = head->next;
+
+    while (fast != NULL)
+    {
+        fast = fast->next;
+        if (fast != NULL)
+        {
+            fast = fast->next;
+        }
+        slow = slow->next;
+    }
+    return slow;
+}
+
+/*
+
+int getLength(Node *head) {
+    int length = 0;
+    while (head != NULL) {
+        length++;
+        head = head->next;
+    }
+    return length;
+}
+
+*/
+
+Node *findMiddle(Node *head) {
+
+    return getMiddle(head);
+
+    /*
+
+    int length = getLength(head);
+    int ans = length/2;
+
+    Node* temp = head;
+    int cnt = 0;
+    while(cnt < ans) {
+        temp = temp -> next;
+        cnt++;
+    }
+    return temp;
 
     */
 }
