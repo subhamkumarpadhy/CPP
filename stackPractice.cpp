ba53f618815dd0eda7eb6c7e2c100acba68283c2
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2023-12-14 22:18:59 +0530

    Done one question based on stack

--- stackPractice.cpp
+++ stackPractice.cpp
@@ -359,3 +359,27 @@ int findMinimumCost(string str)
     int ans = (a + 1) / 2 + (b + 1) / 2;
     return ans;
 }
+
+//Next Smaller Element
+#include <stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int curr = arr[i];
+        while (s.top() >= curr)
+        {
+            s.pop();
+        }
+        // ans is stack ka top
+        ans[i] = s.top();
+        s.push(curr);
+    }
+    return ans;
+}
+
+//Largest Rectangular area in Histogram
