c14d27e70e7b10fc8a16599930d8c447d3c8055d
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2024-01-31 10:53:57 +0530

    Solve indentation issue.

--- OOPS_DAY1.cpp
+++ OOPS_DAY1.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
 
 Q. What is an object?
 -> An Object is an entity which have 2 things such as: 1) State/Property
@@ -12,7 +12,7 @@ Q. How to access the Data members?
 
 --> There are 3 access modifier in Cpp : 1) public - we can access the data members from anywhere.
                                         2) private - we can access the data members only inside the class.
-                                        3) protected - 
+                                        3) protected -
 
 --> We can access or read or manipulate a private data member outside the class with the help of Getter() && Setter().
 
@@ -26,23 +26,23 @@ Q. How to access the Data members?
 --> 'this' is a keyword in cpp , which store the address of current object. So that means we can observe that,
     'this' is a pointer , which points to the address location of the current object.
 
---> Default copy constructor does shallow copy , where as custom copy constructor does Deep copy. In shallow copy we access the 
+--> Default copy constructor does shallow copy , where as custom copy constructor does Deep copy. In shallow copy we access the
     same memory location with 2 different names but in Deep copy another copy is made of the given data & the new variable points
-    to the copied data. 
-    
---> Copy assignment operator "=": If object is already created & now you want to copy the value of one obj to another obj then 
+    to the copied data.
+
+--> Copy assignment operator "=": If object is already created & now you want to copy the value of one obj to another obj then
                                     you will use the copy assignment operator to do so. Ex: a = b that means copy the values of b to a.
-    
+
 --> Destructor = It is used to free the memory. When the scope of a variable ends then the destructor is called to free up the memory.
                 In case of statically created obj the destructor is automatically called but in case of dynamically created obj we have
                 to call the destructor manually.
-    
+
 --> Static keyword: It creates a data member which belongs to a class & to access the member we don't need to create an object.
                     As long as you create a static member you have to initialize it from outside the class.
                     The syntax for initializing it is: [datatype className :: nameOfMember = value;] , where :: -> scope resolution operator
-    
+
 --> Static functions: No need to creating obj for accesing it. We can access it by it's class name only. They don't have 'this' keyword.
-                    It only access the static members only. If we try to access the non-static members in this function then it will 
+                    It only access the static members only. If we try to access the non-static members in this function then it will
                     generate error.
 
 --> Const keyword : It has to be initialized while declearing it. Ex: const int var = 9. If we don't initialize then we will get error.
@@ -55,21 +55,19 @@ Q. How to access the Data members?
 
     */
 
-
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
 class Hero
 {
     // Properties or Data members
-    private:
+private:
     int health;
 
-    public:
+public:
     char level;
     char *name;
     static int complete;
-
     Hero()
     {
         cout << "I'm Constructor. " << endl;
@@ -78,32 +76,32 @@ class Hero
 
     Hero(int health)
     {
-        this -> health = health;
+        this->health = health;
     }
 
-    Hero(int health , char level)
+    Hero(int health, char level)
     {
-        this -> level = level;
-        this -> health = health;
+        this->level = level;
+        this->health = health;
     }
 
     // If we write manual copy constructor then the automatic copy constructor will die.
-    Hero(Hero& mom)
+    Hero(Hero &mom)
     {
         char *ch = new char[strlen(mom.name) + 1];
         strcpy(ch, mom.name);
-        this -> name = ch;
+        this->name = ch;
         cout << "Manual copy constructor called." << endl;
-        this -> health = mom.health;
-        this -> level = mom.level;
+        this->health = mom.health;
+        this->level = mom.level;
     }
 
     void print()
     {
         cout << endl;
-        cout << " [ Health: " << this -> health << ", ";
-        cout << "Level: " << this -> level << ", ";
-        cout << "Name: " << this -> name << " ]";
+        cout << " [ Health: " << this->health << ", ";
+        cout << "Level: " << this->level << ", ";
+        cout << "Name: " << this->name << " ]";
         cout << endl << endl;
     }
 
@@ -127,7 +125,7 @@ class Hero
 
     void setName(char name[])
     {
-        strcpy(this -> name, name);
+        strcpy(this->name, name);
     }
 
     // Creating a static function
@@ -141,7 +139,6 @@ class Hero
     {
         cout << "Hi, I'm Destructor." << endl;
     }
-
 };
 
 // Initializing the static member
@@ -152,31 +149,20 @@ int main()
 
     // Accessing the static function
     cout << Hero::solve << endl;
-
-
-
     // Accessing the static member
     cout << Hero::complete << endl;
-
-
-
-/* 
-
+/*
     // Concept of Destructor
 
     // Static
     Hero a;
-
     // Dynamic
     Hero *b = new Hero();
     delete b;
 
-    */
+*/
 
-
-
-/* 
-
+/*
     // Concept of shallow & deep copy
 
     Hero h1;
@@ -198,9 +184,7 @@ int main()
     // So as h1 changes so h2 also changes.
     // So this concept is known is shallow copy.
 
-    */
-
-
+*/
 
 /*
 
@@ -213,21 +197,16 @@ int main()
 
 */
 
-
-
     /*
-    
+
     Hero kanha(89);
     cout << kanha.getHealth() << endl;
-
 
     Hero *sri = new Hero(45, 'S');
     cout << sri -> getHealth() << endl;
     cout << sri -> getLevel() << endl;
 
     */
-
-
 
     /*
 
@@ -262,8 +241,6 @@ int main()
 
     */
 
-
-    
     /*
 
     subham.health = 98;
@@ -278,7 +255,5 @@ int main()
     cout << "Size : " << sizeof(subham) << endl;
 
     */
-
-    
     return 0;
 }
