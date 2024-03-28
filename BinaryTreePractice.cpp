975a60e6fed89dab26c70e082edff7be85f04565
Subham-Kumar-Padhy <subhamkumarpadhy14@gmail.com>
2024-03-28 21:24:54 +0530

    Practice 2 more questions

--- BinaryTreePractice.cpp
+++ BinaryTreePractice.cpp
@@ -180,3 +180,104 @@ public:
         return isBalancedFast(root).first;
     }
 };
+
+
+// Q. Determine if Two Trees are Identical
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution
+{
+    public:
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //base cases
+        if(r1 == NULL && r2 == NULL) {
+            return true;
+        }
+        if(r1 == NULL && r2 != NULL) {
+            return false;
+        }
+        if(r1 != NULL && r2 == NULL) {
+            return false;
+        }
+        
+        bool left = isIdentical(r1 -> left, r2 -> left);
+        bool right = isIdentical(r1 -> right, r2 -> right);
+        
+        bool value = r1 -> data == r2 -> data;
+        
+        if(left && right && value) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+
+// Q.Sum Tree
+
+/*
+struct Node
+{
+    int data;
+    Node* left, * right;
+};
+*/
+
+class Solution
+{
+    public:
+    pair<bool, int> isSumTreeFast(Node* root) {
+        //base case
+        if(root == NULL) {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        
+        if(root -> left == NULL && root -> right == NULL) {
+            pair<bool, int> p = make_pair(true, root -> data);
+            return p;
+        }
+        
+        pair<bool, int> leftAns = isSumTreeFast(root -> left);
+        pair<bool, int> rightAns = isSumTreeFast(root -> right);
+        
+        bool isLeftSumTree = leftAns.first;
+        bool isRightSumTree = rightAns.first;
+        
+        int leftSum = leftAns.second;
+        int rightSum = rightAns.second;
+        
+        bool condn = root -> data == leftSum + rightSum;
+        
+        pair<bool, int> ans;
+        
+        if(isLeftSumTree && isRightSumTree && condn) {
+            ans.first = true;
+            ans.second = root -> data + leftSum + rightSum;
+        }
+        else {
+            ans.first = false;
+        }
+        return ans;
+    }
+    bool isSumTree(Node* root)
+    {
+        return isSumTreeFast(root).first;
+    }
+};
